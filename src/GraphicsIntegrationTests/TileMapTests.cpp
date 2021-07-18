60c4135d1b2fa38c6b2225c65d935791a7535743
Jon Kuhn <jonkuhn@gmail.com>
2021-07-17 21:26:23 -0400

    Finish first TileMap integration test and flip y axis for tile maps.

--- src/GraphicsIntegrationTests/TileMapTests.cpp
+++ src/GraphicsIntegrationTests/TileMapTests.cpp
@@ -16,15 +16,38 @@ using namespace Graphics;
 
 namespace
 {
+    static Pixel ColorBackgroundUglyYellow(0x80, 0x80, 0x00, 0xff);
+
+    static Pixel ColorWhite(0xff, 0xff, 0xff, 0xff);
+    static Pixel ColorRed(0xff, 0x00, 0x00, 0xff);
+    static Pixel ColorGreen(0x00, 0xff, 0x00, 0xff);
+    static Pixel ColorBlue(0x00, 0x00, 0xff, 0xff);
+
+    static Pixel ColorBlack(0x00, 0x00, 0x00, 0xff);
+    static Pixel ColorYellow(0xff, 0xff, 0x00, 0xff);
+    static Pixel ColorCyan(0x00, 0xff, 0xff, 0xff);
+    static Pixel ColorMagenta(0xff, 0x00, 0xff, 0xff);
+
+    static Pixel ColorDarkGray(0x44, 0x44, 0x44, 0xff);
+    static Pixel ColorLightPurple(0xcc, 0xcc, 0xff, 0xff);
+    static Pixel ColorLightGreen(0xcc, 0xff, 0xcc, 0xff);
+    static Pixel ColorLightPeach(0xff, 0xcc, 0xcc, 0xff);
+
+    static Pixel ColorLightGray(0xcc, 0xcc, 0xcc, 0xff);
+    static Pixel ColorPurple(0xaa, 0x00, 0xff, 0xff);
+    static Pixel ColorSkyBlue(0x00, 0xaa, 0xff, 0xff);
+    static Pixel ColorOrange(0xff, 0xaa, 0x00, 0xff);
+
     class TestTileMapImage : public IImage
     {
     private:
         static constexpr int WIDTH = 8;
         static constexpr int HEIGHT = 4;
-        typedef std::array<std::array<std::array<uint8_t, 4>, WIDTH>, HEIGHT> PixelsType;
+        typedef std::array<std::array<std::array<uint8_t, 4>, WIDTH>, HEIGHT> TileIndicesType;
+        typedef std::array<std::array<Pixel, WIDTH>, HEIGHT> TileColorsType;
 
         // 8x4x4 array
-        PixelsType _pixels;
+        TileIndicesType _tiles;
 
     public:
         static constexpr std::array<uint8_t, 4> TileWhite = { 0, 0, 0, 0 };
@@ -47,12 +70,20 @@ namespace
         static constexpr std::array<uint8_t, 4> TileSkyBlue   = { 2, 3, 0, 0 };
         static constexpr std::array<uint8_t, 4> TileOrange    = { 3, 3, 0, 0 };
 
+        TileColorsType TileColors;
+
         TestTileMapImage()
-            : _pixels({ {
+            : _tiles({ {
                 { TileYellow, TileGreen, TileCyan, TileRed, TileLightGreen, TilePurple, TileLightPurple, TileSkyBlue },
                 { TileOrange, TileWhite, TileBlue, TileLightGray, TileLightPeach, TileBlack, TileMagenta, TileDarkGray },
                 { TileWhite, TileBlue, TileLightGray, TileLightPeach, TileBlack, TileMagenta, TileDarkGray, TileOrange },
                 { TileGreen, TileCyan, TileRed, TileLightGreen, TilePurple, TileLightPurple, TileSkyBlue, TileYellow }
+            } }),
+              TileColors({ {
+                { ColorYellow, ColorGreen, ColorCyan, ColorRed, ColorLightGreen, ColorPurple, ColorLightPurple, ColorSkyBlue },
+                { ColorOrange, ColorWhite, ColorBlue, ColorLightGray, ColorLightPeach, ColorBlack, ColorMagenta, ColorDarkGray },
+                { ColorWhite, ColorBlue, ColorLightGray, ColorLightPeach, ColorBlack, ColorMagenta, ColorDarkGray, ColorOrange },
+                { ColorGreen, ColorCyan, ColorRed, ColorLightGreen, ColorPurple, ColorLightPurple, ColorSkyBlue, ColorYellow }
             } })
         {
 
@@ -60,8 +91,9 @@ namespace
 
         const uint8_t* Data() const override
         {
-            return &_pixels[0][0][0];
+            return &_tiles[0][0][0];
         }
+
         int Width() const override
         {
             return WIDTH;
@@ -76,8 +108,41 @@ namespace
         {
             return PixelFormat::RGBA;
         }
-
     };
+
+    template<unsigned int ColumnCount, unsigned int RowCount>
+    void ExpectTileColorsInScreenshot(
+        IScreenshot& screenshot,
+        std::array<std::array<Pixel, ColumnCount>, RowCount> expectedTileColors)
+    {
+        auto columnWidth = screenshot.Width() / ColumnCount;
+        auto rowHeight = screenshot.Height() / RowCount;
+        auto sampleDistanceFromEdgeX = columnWidth / 16;
+        auto sampleDistanceFromEdgeY = rowHeight / 16;
+
+        // sample each tile near 4 corners plus center
+        auto sampleLeftX = sampleDistanceFromEdgeX;
+        auto sampleUpperY = sampleDistanceFromEdgeY;
+        auto sampleRightX = columnWidth - sampleDistanceFromEdgeX;
+        auto sampleLowerY = rowHeight - sampleDistanceFromEdgeY;
+        auto sampleCenterX = columnWidth / 2;
+        auto sampleCenterY = rowHeight / 2;
+        
+        for(unsigned int row = 0; row < RowCount; row++)
+        {
+            for(unsigned int column = 0; column < ColumnCount; column++)
+            {
+                auto expectedColor = expectedTileColors[row][column];
+                auto x = column * columnWidth;
+                auto y = row * rowHeight;
+                EXPECT_EQ(expectedColor, screenshot.GetPixel(x + sampleLeftX, y + sampleUpperY));
+                EXPECT_EQ(expectedColor, screenshot.GetPixel(x + sampleRightX, y + sampleUpperY));
+                EXPECT_EQ(expectedColor, screenshot.GetPixel(x + sampleCenterX, y + sampleCenterY));
+                EXPECT_EQ(expectedColor, screenshot.GetPixel(x + sampleLeftX, y + sampleLowerY));
+                EXPECT_EQ(expectedColor, screenshot.GetPixel(x + sampleRightX, y + sampleLowerY));
+            }
+        }
+    }
 }
 
 class TileMapTests : public Test
@@ -124,13 +189,59 @@ TEST_F(TileMapTests, GivenSolidColored4x4TileAtlas_AssignedInDifferentOrderIn16x
         -1.0f,
         1.0f);
 
-    while (_engine->Update())
-    {
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        tileMap->Draw(model, view, projection);
-    }
+    glClearColor(0.5f, 0.5f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    tileMap->Draw(model, view, projection);
 
-    auto screenshot = _engine->TakeScreenshot();
-    screenshot->GetPixel(100, 100);
+    auto scr = _engine->TakeScreenshot();
+
+    const unsigned int ColumnCount = 8;
+    const unsigned int RowCount = 6;
+    ExpectTileColorsInScreenshot<ColumnCount, RowCount>(
+        *scr,
+        std::array<std::array<Pixel, ColumnCount>, RowCount>(
+        { {
+            // First row is background color
+            { ColorBackgroundUglyYellow, ColorBackgroundUglyYellow, ColorBackgroundUglyYellow, ColorBackgroundUglyYellow,
+            ColorBackgroundUglyYellow, ColorBackgroundUglyYellow, ColorBackgroundUglyYellow, ColorBackgroundUglyYellow },
+
+            // Middle 4 rows should be equal to the colors of the tiles in the tile atlas
+            // as selected by TestTileMapImage
+            testTileMapImage.TileColors[0],
+            testTileMapImage.TileColors[1],
+            testTileMapImage.TileColors[2],
+            testTileMapImage.TileColors[3],
+
+            // Last row is background color
+            { ColorBackgroundUglyYellow, ColorBackgroundUglyYellow, ColorBackgroundUglyYellow, ColorBackgroundUglyYellow,
+            ColorBackgroundUglyYellow, ColorBackgroundUglyYellow, ColorBackgroundUglyYellow, ColorBackgroundUglyYellow }
+        } }));
+/*
+    auto columnWidth = scr->Width() / columnCount;
+    auto rowHeight = scr->Height() / rowCount;
+    auto sampleDistanceFromEdgeX = columnWidth / 16;
+    auto sampleDistanceFromEdgeY = rowHeight / 16;
+
+    unsigned int sampleLeftX = sampleDistanceFromEdgeX;
+    unsigned int sampleUpperY = sampleDistanceFromEdgeY;
+    unsigned int sampleRightX = columnWidth - sampleDistanceFromEdgeX;
+    unsigned int sampleLowerY = rowHeight - sampleDistanceFromEdgeY;
+    unsigned int sampleCenterX = columnWidth / 2;
+    unsigned int sampleCenterY = rowHeight / 2;
+    
+    for(unsigned int row = 0; row < rowCount; row++)
+    {
+        for(unsigned int column = 0; column < columnCount; column++)
+        {
+            auto expectedColor = expectedTileColors[row][column];
+            auto x = column * columnWidth;
+            auto y = row * rowHeight;
+            EXPECT_EQ(expectedColor, scr->GetPixel(x + sampleLeftX, y + sampleUpperY));
+            EXPECT_EQ(expectedColor, scr->GetPixel(x + sampleRightX, y + sampleUpperY));
+            EXPECT_EQ(expectedColor, scr->GetPixel(x + sampleCenterX, y + sampleCenterY));
+            EXPECT_EQ(expectedColor, scr->GetPixel(x + sampleLeftX, y + sampleLowerY));
+            EXPECT_EQ(expectedColor, scr->GetPixel(x + sampleRightX, y + sampleLowerY));
+        }
+    }
+    */
 }
