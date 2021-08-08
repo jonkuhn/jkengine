dfbc57de1eaafd9d6028b5418bbf0bd2adbefc6e
Jon Kuhn <jonkuhn@gmail.com>
2021-08-08 16:21:49 -0400

    Change TileMapDrawer and SpriteDrawer constructor parameters from pointers to references
    This better communicates that it is not valid to pass nullptr for these
    parameters.
    
    This also makes these classes not copyable or movable because copy and move are
    not necessary.  If they become necessary then the members can be changed to
    pointers but the constructor parameters should stay as references.

--- src/Graphics/OpenGL/SpriteDrawer.h
+++ src/Graphics/OpenGL/SpriteDrawer.h
@@ -12,9 +12,9 @@ namespace Graphics::OpenGL
     {
     public:
         SpriteDrawer(
-            SpriteShaderProgram* spriteShaderProgram,
-            UnitQuadVertexArray* unitQuadVertexArray,
-            Camera2d* camera2d)
+            SpriteShaderProgram& spriteShaderProgram,
+            UnitQuadVertexArray& unitQuadVertexArray,
+            Camera2d& camera2d)
 
           : _spriteShaderProgram(spriteShaderProgram),
             _unitQuadVertexArray(unitQuadVertexArray),
@@ -23,24 +23,36 @@ namespace Graphics::OpenGL
 
         }
 
+        // To not allow copy or move because they are unnecessary
+        // for this class.  At least right now, there is only a
+        // need for one instance anyway.  If we want them to be
+        // copyable or movable in the future the members can change
+        // to pointers rather than references, but constructor
+        // arguments for the constructor above should stay as
+        // references since passing nullptr is not valid.
+        SpriteDrawer(const SpriteDrawer& other) = delete;
+        SpriteDrawer& operator=(const SpriteDrawer& other) = delete;
+        SpriteDrawer(SpriteDrawer&& other) = delete;
+        SpriteDrawer& operator=(SpriteDrawer&& other) = delete;
+
         inline void SetupForDrawingFromAtlas(const TileAtlas& atlas)
         {
-            _spriteShaderProgram->Use();
-            _spriteShaderProgram->Atlas(atlas);
-            _spriteShaderProgram->ViewMatrix(_camera2d->ViewMatrix());
-            _spriteShaderProgram->ProjectionMatrix(_camera2d->ProjectionMatrix());
+            _spriteShaderProgram.Use();
+            _spriteShaderProgram.Atlas(atlas);
+            _spriteShaderProgram.ViewMatrix(_camera2d.ViewMatrix());
+            _spriteShaderProgram.ProjectionMatrix(_camera2d.ProjectionMatrix());
         }
 
         inline void Draw(const Sprite& sprite)
         {
-            _spriteShaderProgram->AtlasLocation(sprite.AtlasLocation());
-            _spriteShaderProgram->ModelMatrix(sprite.ModelMatrix());
-            _unitQuadVertexArray->Draw();
+            _spriteShaderProgram.AtlasLocation(sprite.AtlasLocation());
+            _spriteShaderProgram.ModelMatrix(sprite.ModelMatrix());
+            _unitQuadVertexArray.Draw();
         }
 
     private:
-        SpriteShaderProgram* _spriteShaderProgram;
-        UnitQuadVertexArray* _unitQuadVertexArray;
-        Camera2d* _camera2d;
+        SpriteShaderProgram& _spriteShaderProgram;
+        UnitQuadVertexArray& _unitQuadVertexArray;
+        Camera2d& _camera2d;
     };
 }
