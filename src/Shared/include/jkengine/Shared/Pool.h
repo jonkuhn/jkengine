fd65acb0bc6778fcbfbbb56957fb6447dfff8060
Jon Kuhn <jonkuhn@gmail.com>
2022-09-02 22:46:03 -0400

    Extract MemoryChunk class template

--- src/Shared/include/jkengine/Shared/Pool.h
+++ src/Shared/include/jkengine/Shared/Pool.h
@@ -58,43 +58,164 @@ namespace Shared
         PoolUniquePtr() = delete;
     };
 
+    template<typename T, size_t CapacityPerChunk>
+    class MemoryChunk
+    {
+    public:
+        MemoryChunk()
+          : _isIndexAllocated(),
+            _memoryChunk(new (std::align_val_t(alignof(T))) uint8_t[sizeof(T) * CapacityPerChunk]),
+            _objects(reinterpret_cast<T*>(_memoryChunk))
+        {
+
+        }
+
+        ~MemoryChunk()
+        {
+            if (_memoryChunk != nullptr)
+            {
+                ::operator delete[] (static_cast<uint8_t*>(_memoryChunk), std::align_val_t(alignof(T))) ;
+            }
+        }
+
+        // Non-copyable
+        MemoryChunk(const MemoryChunk&) = delete;
+        MemoryChunk& operator=(const MemoryChunk&) = delete;
+
+        MemoryChunk(MemoryChunk&& other) noexcept
+          : _isIndexAllocated(std::move(other._isIndexAllocated)),
+            _memoryChunk(std::exchange(other._memoryChunk, nullptr)),
+            _objects(std::exchange(other._objects, nullptr))
+        {
+
+        };
+
+        MemoryChunk& operator=(MemoryChunk&& other) noexcept
+        {
+            std::swap(_isIndexAllocated, other._isIndexAllocated);
+            std::swap(_memoryChunk, other._memoryChunk);
+            std::swap(_objects, other._objects);
+            return *this;
+        };
+
+        inline T* ObjectAt(size_t index)
+        {
+            if (_isIndexAllocated[index])
+            {
+                return &(_objects[index]);
+            }
+            return nullptr;
+        }
+
+        inline size_t CountAllocatedIndices()
+        {
+            return _isIndexAllocated.count();
+        }
+
+        inline bool IsPointerInRange(void* pointer)
+        {
+            uint8_t* pointerAsBytePtr = static_cast<uint8_t*>(pointer);
+            uint8_t* startOfChunk = _memoryChunk;
+            uint8_t* pastEndOfChunk = startOfChunk + CapacityPerChunk * sizeof(T);
+
+            if ((pointerAsBytePtr >= startOfChunk)
+                && ((pointerAsBytePtr + sizeof(T)) <= pastEndOfChunk))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        template<typename TInterface = T, typename ... Args>
+        inline typename PoolUniquePtr<TInterface>::T MakeUnique(IDestroyer& destroyer, Args&&... args)
+        {
+            void* memoryForObj = TryToFindMemoryForNewObject();
+            if (memoryForObj == nullptr)
+            {
+                return nullptr;
+            }
+
+            typename PoolUniquePtr<TInterface>::T objUniquePointer(
+                new (memoryForObj) T(std::forward<Args>(args)...),
+                PoolDeleter(destroyer));
+
+            return objUniquePointer;
+        }
+
+        inline void Destroy(T* object)
+        {
+            object->~T();
+
+            uint8_t* pointerAsBytePtr = reinterpret_cast<uint8_t*>(object);
+            uint8_t* startOfChunk = _memoryChunk;
+            size_t index = (pointerAsBytePtr - startOfChunk) / sizeof(T);
+            _isIndexAllocated[index] = false;
+        }
+
+        inline std::string DebugString(void* pointer)
+        {
+            uint8_t* pointerAsBytePtr = static_cast<uint8_t*>(pointer);
+            uint8_t* startOfChunk = _memoryChunk;
+            uint8_t* pastEndOfChunk = startOfChunk + CapacityPerChunk * sizeof(T);
+            size_t index = (pointerAsBytePtr - startOfChunk) / sizeof(T);
+
+            std::stringstream ss;
+            ss << " MemoryChunk(_objects = 0x" << std::hex << reinterpret_cast<uintptr_t>(_objects)
+                << ", startOfChunk = 0x" << std::hex << reinterpret_cast<uintptr_t>(startOfChunk)
+                << ", pastEndOfChunk = 0x" << std::hex << reinterpret_cast<uintptr_t>(pastEndOfChunk)
+                << ", index = " << std::dec << index << ")";
+
+            return ss.str();
+        }
+
+    private:
+        inline void* TryToFindMemoryForNewObject()
+        {
+            for (size_t index = 0; index < CapacityPerChunk; index++)
+            {
+                if (!_isIndexAllocated[index])
+                {
+                    _isIndexAllocated[index] = true;
+                    return &_objects[index];
+                }
+            }
+
+            return nullptr;
+        }
+
+        std::bitset<CapacityPerChunk> _isIndexAllocated;
+        uint8_t* _memoryChunk;
+        T* _objects;
+    };
+
     template<typename T, size_t CapacityPerChunk = 128>
     class Pool final : public IDestroyer
     {
     public:
         Pool()
           : _mutex(),
-            _isChunkIndexAllocated(),
-            _memoryChunks(),
-            _chunkObjects()
+            _memoryChunks(1)
         {
-            AllocateNewChunk();
+
         }
 
         ~Pool()
         {
+            std::scoped_lock<std::mutex> lock(_mutex);
             size_t countStillAllocated = 0;
-            for(auto& isIndexAllocated : _isChunkIndexAllocated)
+            for(auto& memoryChunk : _memoryChunks)
             {
-                if(isIndexAllocated.any())
-                {
-                    countStillAllocated += isIndexAllocated.count();
-                }
+                countStillAllocated += memoryChunk.CountAllocatedIndices();
             }
 
-            if (countStillAllocated)
+            if (countStillAllocated > 0)
             {
                 std::cerr << "FATAL: Pool destructor called with "
                     << countStillAllocated << " objects still registered."
                     << std::endl;
                 std::abort();
             }
-
-            for(auto* memoryChunk : _memoryChunks)
-            {
-                ::operator delete[] (static_cast<uint8_t*>(memoryChunk), std::align_val_t(alignof(T))) ;
-            }
-
         }
 
         // Do not allow copy.  If it were copyable then PoolDeleter
@@ -111,53 +232,37 @@ namespace Shared
         template<typename TInterface = T, typename ... Args>
         inline typename PoolUniquePtr<TInterface>::T MakeUnique(Args&&... args)
         {
-            void* memoryForObj = TryToFindMemoryForNewObject();
-            if (memoryForObj == nullptr)
+            std::scoped_lock<std::mutex> lock(_mutex);
+            for (auto& memoryChunk : _memoryChunks)
             {
-                AllocateNewChunk();
-
-                // TODO: optimize to not search again
-                memoryForObj = TryToFindMemoryForNewObject();
+                auto newObject = memoryChunk.template MakeUnique<TInterface>(*this, std::forward<Args>(args)...);
+                if (newObject)
+                {
+                    return newObject;
+                }
             }
 
-            // TODO: consider clearing _isIndexAllocated[i] if the following
-            // throws by using an RAII object to set _isIndexAllocated[i]
-
-            typename PoolUniquePtr<TInterface>::T objUniquePointer(
-                new (memoryForObj) T(std::forward<Args>(args)...),
-                PoolDeleter(*this));
-
-            return objUniquePointer;
+            _memoryChunks.emplace_back();
+            return _memoryChunks.back().template MakeUnique<TInterface>(*this, std::forward<Args>(args)...);
         }
 
         void Destroy(void* pointer) override
         {
+            std::scoped_lock<std::mutex> lock(_mutex);
             if (pointer == nullptr)
             {
                 std::cerr << "FATAL: Pool::Destroy called with nullptr" << std::endl;
                 std::abort();
             }
 
-            for(size_t chunk = 0; chunk < _memoryChunks.size(); chunk++)
+            for (auto& memoryChunk : _memoryChunks)
             {
-                auto* pointerAsBytePtr = static_cast<uint8_t*>(pointer);
-                auto* startOfChunk = _memoryChunks[chunk];
-                auto* pastEndOfChunk = startOfChunk + CapacityPerChunk * sizeof(T);
-                size_t index = (pointerAsBytePtr - startOfChunk) / sizeof(T);
-
-                if ((pointerAsBytePtr < startOfChunk)
-                    || ((pointerAsBytePtr + sizeof(T) - 1) >= pastEndOfChunk)
-                    || index >= CapacityPerChunk)
+                if (!memoryChunk.IsPointerInRange(pointer))
                 {
                     // keep looking for the right chunk
                     continue;
                 }
-
-                T& obj = *static_cast<T*>(pointer);
-                obj.~T();
-
-                std::scoped_lock<std::mutex> lock(_mutex);
-                _isChunkIndexAllocated[chunk][index] = false;
+                memoryChunk.Destroy(static_cast<T*>(pointer));
                 return;
             }
 
@@ -165,19 +270,12 @@ namespace Shared
             ss << "Destroy called for object that is out of range: "
                 << " pointer = 0x" << std::hex << reinterpret_cast<uintptr_t>(pointer)
                 << " sizeof(T) = " << std::dec << sizeof(T)
-                << " alignof(T) = " << std::dec << alignof(T);
+                << " alignof(T) = " << std::dec << alignof(T)
+                << std::endl;
 
-            for(size_t chunk = 0; chunk < _memoryChunks.size(); chunk++)
+            for (auto& memoryChunk : _memoryChunks)
             {
-                auto* pointerAsBytePtr = static_cast<uint8_t*>(pointer);
-                auto* startOfChunk = _memoryChunks[chunk];
-                auto* pastEndOfChunk = startOfChunk + CapacityPerChunk * sizeof(T);
-                size_t index = (pointerAsBytePtr - startOfChunk) / sizeof(T);
-
-                ss << " _chunkObjects[" << std::dec << chunk << "] = 0x" << std::hex << reinterpret_cast<uintptr_t>(_chunkObjects[chunk])
-                    << " startOfChunk[" << std::dec << chunk << "] = 0x" << std::hex << reinterpret_cast<uintptr_t>(startOfChunk)
-                    << " pastEndOfChunk[" << std::dec << chunk << "] = 0x" << std::hex << reinterpret_cast<uintptr_t>(pastEndOfChunk)
-                    << " indexInChunk[" << std::dec << chunk << "] = " << std::dec << index;
+                ss << memoryChunk.DebugString(pointer) << std::endl;
             }
 
             throw std::out_of_range(ss.str().c_str());
@@ -186,73 +284,40 @@ namespace Shared
         typedef std::function<void(T&)> ForEachCallback;
         inline void ForEach(ForEachCallback callback)
         {
-            // TODO: do we need to make sure _memoryChunks.size() gets re-evaluated each
-            // loop and also should it be wrapped in a lock?
-            for(size_t chunk = 0; chunk < _memoryChunks.size(); chunk++)
+            // This is safe because we never decrease the size of 
+            // _memoryChunks so all indices less than this will be
+            // valid.
+            size_t memoryChunkCount = 0;
             {
-                for (size_t i = 0; i < CapacityPerChunk; i++)
+                std::scoped_lock<std::mutex> lock(_mutex);
+                memoryChunkCount = _memoryChunks.size();
+            }
+
+            for (size_t chunk = 0; chunk < memoryChunkCount; chunk++)
+            {
+                for (size_t index = 0; index < CapacityPerChunk; index++)
                 {
                     T* obj = nullptr;
                     {
                         std::scoped_lock<std::mutex> lock(_mutex);
-                        if (!_isChunkIndexAllocated[chunk][i])
+                        obj = _memoryChunks[chunk].ObjectAt(index);
+
+                        if (obj == nullptr)
                         {
+                            // no object at the given index, keep looking
                             continue;
                         }
-                        obj = &_chunkObjects[chunk][i];
                     }
+
+                    // Objects allocated during the callback may or may not
+                    // have the callback called for them.
                     callback(*obj);
                 }
             }
         }
 
-    private:
-        void AllocateNewChunk()
-        {
-            _isChunkIndexAllocated.emplace_back();
-
-            uint8_t* newMemoryChunk = new (std::align_val_t(alignof(T))) uint8_t[sizeof(T) * CapacityPerChunk];
-            _memoryChunks.push_back(newMemoryChunk);
-
-            _chunkObjects.push_back({
-                reinterpret_cast<T*>(newMemoryChunk)
-            });
-
-            if(!((_isChunkIndexAllocated.size() == _memoryChunks.size())
-                && (_memoryChunks.size() == _chunkObjects.size())))
-            {
-                std::stringstream ss;
-                ss << "Invariant violated not all chunk related vectors have the same size "
-                    << " _isChunkIndexAllocated.size() = " << _isChunkIndexAllocated.size()
-                    << ", _memoryChunks.size() = " << _memoryChunks.size()
-                    << ", _chunkObjects.size() = " << _chunkObjects.size();
-                throw std::logic_error(ss.str().c_str());
-            }
-        }
-
-        void* TryToFindMemoryForNewObject()
-        {
-            std::scoped_lock<std::mutex> lock(_mutex);
-
-            for(size_t chunk = 0; chunk < _isChunkIndexAllocated.size(); chunk++)
-            {
-                for (size_t i = 0; i < CapacityPerChunk; i++)
-                {
-                    if (!_isChunkIndexAllocated[chunk][i])
-                    {
-                        _isChunkIndexAllocated[chunk][i] = true;
-                        return &_chunkObjects[chunk][i];
-                    }
-                }
-            }
-
-            return nullptr;
-        }
-
         std::mutex _mutex;
-        std::vector<std::bitset<CapacityPerChunk>> _isChunkIndexAllocated;
-        std::vector<uint8_t*> _memoryChunks;
-        std::vector<T*> _chunkObjects;
+        std::vector<MemoryChunk<T, CapacityPerChunk>> _memoryChunks;
     };
 }
 
